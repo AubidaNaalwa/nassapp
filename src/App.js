d4f1e9681cbb481627816c670f30a5fa6824c3ec
Oryam <48219965+or-yam@users.noreply.github.com>
2020-08-16 20:17:33 +0300

    nassap init, fetching from api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
+const API = 'P84jqZfmLAUJMami5uAzyephI2upidRPhytcdMKD';
+
 function App() {
+  const [imageOfTheDay, seImageOfTheDay] = useState({});
+
+  useEffect(() => {
+    axios(`https://api.nasa.gov/planetary/apod?api_key=${API}`).then((res) =>
+      seImageOfTheDay(res.data)
+    );
+  }, []);
+  console.log(imageOfTheDay);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{imageOfTheDay.title}</h1>
+      <h3>{imageOfTheDay.date}</h3>
+      <img src={imageOfTheDay.url} alt="" />
+      <p>{imageOfTheDay.explanation}</p>
     </div>
   );
 }
