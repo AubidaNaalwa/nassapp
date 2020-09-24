9f5311a8547426dbd4b6af0a4cd350f7e61b8bde
Oryam <48219965+or-yam@users.noreply.github.com>
2020-09-24 11:28:30 +0300

    fixed db uri

--- Server/server.js
+++ Server/server.js
@@ -20,10 +20,13 @@ app.use(function (req, res, next) {
 });
 /* End of Development section  */
 
-mongoose.connect(`${process.env.MONGO_URI}`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(`${process.env.MONGO_URI}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((connect) => console.log('connected to mongodb..'))
+  .catch((e) => console.log('could not connect to mongodb', e));
 
 app.use(express.static(path.join(__dirname, '../build')));
 app.use(bodyParser.json());
